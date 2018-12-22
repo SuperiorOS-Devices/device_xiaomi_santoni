63d92e25a3ee7098bd77a0f32790841803a038b3
Bruno Martins <bgcngm@gmail.com>
2018-12-22 19:07:24 +0000

    santoni: Enable B-services aging propagation
    Change-Id: I4ac0525933bb421f7d025c5e4c6a1de0f083c46e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -138,6 +138,10 @@ vendor.vidc.disable.split.mode=1 \
 vendor.vidc.enc.disable.pq=true \
 video.disable.ubwc=1
 
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.qti.sys.fw.bservice_enable=true
+
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.sys.fw.dex2oat_thread_count=4 \
