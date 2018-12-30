564e3baf0c56843976499d1ddc49b28e43ecc8d8
Bruno Martins <bgcngm@gmail.com>
2018-12-30 19:58:56 +0000

    santoni: Convert XiaomiDoze into XiaomiParts
     * This is the preparation for turning the package into
       an unified device-settings app.
    
    Change-Id: I425ad525edcc63c2dcccd1e2fb8459f442744970
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -67,9 +67,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Ambient Display
+# Device-specific Settings
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    XiaomiParts
 
 # Audio
 PRODUCT_PACKAGES += \
