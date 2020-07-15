8547b1ed5e574811143560cdd42a2f0bbdc599bb
bikram557 <kitkatmukherjee2015@gmail.com>
2020-07-15 18:53:42 +0000

    santoni: Rebase SuperiorOS Tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_santoni.mk
+    $(LOCAL_DIR)/superior_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_santoni-userdebug
+    superior_santoni-userdebug
--- superior_santoni.mk
+++ superior_santoni.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The Havoc-OS Project
+# Copyright (C) 2019 The SuperiorOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,27 +16,25 @@
 
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-# Inherit some common Revenge OS stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 PRODUCT_DEVICE := santoni
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4X
-PRODUCT_NAME := derp_santoni
+PRODUCT_NAME := superior_santoni
 BOARD_VENDOR := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Official
-DERP_BUILDTYPE=Official
+#OFFICIAL
+export SUPERIOR_OFFICIAL=true
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.superior.maintainer=Bikram-Mukherjee
 
 # Boot animation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-#GApps
-DERP_BUILD_ZIP_TYPE := GAPPS
-IS_PHONE := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_MINIMAL_APPS := false
+TARGET_BOOT_ANIMATION_RES := 720
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
